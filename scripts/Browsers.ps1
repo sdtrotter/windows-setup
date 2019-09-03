8cec03286f6368b016a27db5246d2af332998989
Scott Trotter <scott@trotternet.com>
2019-09-03 14:55:17 -0700

    added several new helper scripts

--- scripts/Browsers.ps1
+++ scripts/Browsers.ps1
@@ -1,3 +1,11 @@
 #--- Browsers ---
 choco install -y googlechrome
 choco install -y firefox
+#choco install -y edge                  # no public package (yet)
+#choco install -y brave
+#choco install -y opera
+#choco install -y chromium
+choco install -y tor-browser
+
+# browser extensions
+choco install -y lastpass

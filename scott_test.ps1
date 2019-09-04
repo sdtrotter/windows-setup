ec3c8af2aa34d553be19a77629a5b4a996b7720c
Scott Trotter <scott@trotternet.com>
2019-09-03 19:04:42 -0700

    disabled most scripts for testing

--- scott_test.ps1
+++ scott_test.ps1
@@ -23,17 +23,17 @@ function executeScript {
 
 #--- Setting up Windows ---
 #executeScript "RemoveDefaultApps.ps1";
-executeScript "HyperV.ps1";
-executeScript "SystemConfiguration.ps1";
-executeScript "FileExplorerSettings.ps1";
-executeScript "SystemUtilities.ps1";
+#executeScript "HyperV.ps1";
+#executeScript "SystemConfiguration.ps1";
+#executeScript "FileExplorerSettings.ps1";
+#executeScript "SystemUtilities.ps1";
 executeScript "Browsers.ps1";
-executeScript "MediaTools.ps1";
-executeScript "ProductivityApps.ps1";
-executeScript "CommunicationsApps.ps1";
+#executeScript "MediaTools.ps1";
+#executeScript "ProductivityApps.ps1";
+#executeScript "CommunicationsApps.ps1";
 executeScript "FunAndGames.ps1";
 #executeScript "CommonDevTools.ps1";
-executeScript "MoreDevTools.ps1";
+#executeScript "MoreDevTools.ps1";
 #executeScript "WSL.ps1";
 #executeScript "Docker.ps1";
 
